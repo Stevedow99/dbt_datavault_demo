e5dda71e8c726c0236611e170384fdb13170f535
Steve Dowling <steve.dowling@fishtownanalytics.com>
2022-11-22 19:11:11 +0000

    updates

---
+++ models/staging/salesforce/stg_salesforce_accounts.sql
@@ -0,0 +1,23 @@
+with source as (
+
+    select * from {{ source('salesforce', 'accounts') }}
+
+),
+
+renamed as (
+
+    select
+    
+        batchid as batchid,
+        companyextid as accountid,
+        company_name,
+        city,
+        state,
+        datecreated
+        modifieddate
+
+    from source
+
+)
+
+select * from renamed
