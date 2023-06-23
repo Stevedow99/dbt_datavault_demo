c9db1179339ffb5c9f24fe91d88098d1cb4a2bad
Sean McIntyre <sean.mcintyre@fishtownanalytics.com>
2023-06-23 12:21:14 +0200

    Get the ERD generator working

--- models/d__business_vault/intermediate_tables/opportunities_accounts_contacts_linker.sql
+++ models/d__business_vault/intermediate_tables/opportunities_accounts_contacts_linker.sql
@@ -5,7 +5,7 @@ Select
     hubo.OPPURTUNITYID,
     linkoa.ACCOUNT_PK_HASH,
     linkca.CONTACT_PK_HASH
-FROM {{ ref('hub_opportunities') }} hubo
+FROM {{ ref('hub_opportunity') }} hubo
     LEFT JOIN {{ ref('link_opportunity_account') }} linkoa
         ON hubo.OPPURTUNITY_PK_HASH = linkoa.OPPURTUNITY_PK_HASH
     LEFT JOIN {{ ref('link_contact_account') }} linkca
